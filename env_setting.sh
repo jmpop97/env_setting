a66e6dbf627c484789f5604a539c62e52c002034
jmpop <pjm970128@gmail.com>
2024-08-14 00:19:44 +0900

    add - .env setting to .gitignore

--- env_setting.sh
+++ env_setting.sh
@@ -1,1 +1,40 @@
-echo ".env" >> .gitignore
+function gitignore_setting(){
+    function empty_check(){
+        if [ -s ".gitignore" ]; then
+            echo "1"
+            return
+        else
+            echo "0"
+            return
+        fi
+    }
+    function combine_check(){
+        while IFS= read -r line; do
+            if [[ "$line" == ".env"* ]]; then
+                echo "1"
+                return
+            fi
+        done < .gitignore
+        if [[ "$line" == ".env"* ]]; then
+            echo "1"
+            return
+        fi
+        echo "0"
+        return
+    }
+
+
+    bool_add=$(empty_check)
+    if [ ${bool_add} -eq "1" ]; then
+        bool_add=$(combine_check)
+    fi
+    echo "$bool_add"
+
+
+    if ! [ ${bool_add} -eq "1" ]; then
+        echo ".env" >> .gitignore
+    fi
+
+
+}
+gitignore_setting
